89bb7631d4789d8b53d22bcff087800627050cd8
Anh <voduyanh1984@gmail.com>
2020-08-27 18:31:54 +0700

    - need to set cookies whenever make request(s)

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,12 @@ const moment = require('moment');
 const baseService = require('../services/baseService');
 const info = require('../package.json');
 
+const userInfo = {
+    CompanyDB: "SBODemoAU",
+    UserName: "manager",
+    Password: "manager"
+};
+
 router.get('/status', function (req, res, next) {
     res.json({
         code: true,
@@ -27,12 +33,35 @@ router.post('/status', function (req, res, next) {
 router.get('/loginsap', async function (req, res, next) {
     try 
     {
-        let data = await baseService.loginSAP();
+        let data = await baseService.loginSAP(userInfo);
         console.log('data:', data);
         
         res.json({
             code: true,     
             data: data,
+            version: info.version,
+            time: moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+        });
+
+        next();
+    } 
+    catch(err) {
+        throw err;
+    }
+});
+
+router.get('/items', async function (req, res, next) {
+    try 
+    {
+        let res_login = await baseService.loginSAP(userInfo);
+        let { SessionId } = res_login;
+        
+        let cookieString = `B1SESSION=${SessionId}`;
+        let res_items = await baseService.getItems(cookieString);
+
+        res.json({
+            code: true,     
+            data: res_items,
             version: info.version,
             time: moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
         });
--- services/baseService.js
+++ services/baseService.js
@@ -4,30 +4,30 @@ const api = require('./api');
 const baseService = function() {    
 }
 
-baseService.loginSAP = function() {
+baseService.loginSAP = function(info) {
     let url = '/b1s/v1/Login';
-    let data = {
-        CompanyDB: "SBODemoAU",
-        UserName: "manager",
-        Password: "manager"
-    };
     return new Promise(function(resolve, reject) {
-        api.post(url, data).then(function(res) {
+        api.post(url, info).then(function(res) {
             let { data } = res;
             resolve(data);
         }).catch(function(err) {
             resolve(err);
-        });    
+        });
     });
 }
 
-baseService.getItems = async function() {
-    let url = '';
+baseService.getItems = async function(cookieString) {
+    let url = '/b1s/v1/Items';
     return new Promise(function(resolve, reject) {
-        request(url, function(err, res, body) {
-            if(err) reject(err);
-            else resolve(body);
-        });
+        api.setCookies(cookieString);
+        api.get(url).then(function(res) {
+            console.log('success');
+            let { data } = res;
+            resolve(data);
+        }).catch(function(err) {
+            console.log('error');
+            resolve(err);
+        });        
     });
 }
 
