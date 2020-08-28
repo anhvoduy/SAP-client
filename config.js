76ae057bc4c36691c14dc68993b1c0a52df99981
Anh <voduyanh1984@gmail.com>
2020-08-28 10:01:23 +0700

    - clean up

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    userInfo: {
+        CompanyDB: "SBODemoAU",
+        UserName: "manager",
+        Password: "manager"
+    }
+}
+
+module.exports = config;
--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,7 @@ const router = require('express').Router();
 const moment = require('moment');
 const baseService = require('../services/baseService');
 const info = require('../package.json');
-
-const userInfo = {
-    CompanyDB: "SBODemoAU",
-    UserName: "manager",
-    Password: "manager"
-};
+const userInfo = require('../config').userInfo;
 
 router.get('/status', function (req, res, next) {
     res.json({
@@ -34,14 +29,12 @@ router.get('/loginsap', async function (req, res, next) {
     try 
     {
         let data = await baseService.loginSAP(userInfo);
-        
         res.json({
             code: true,     
             data: data,
             version: info.version,
             time: moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
         });
-
         next();
     } 
     catch(err) {
@@ -53,18 +46,17 @@ router.get('/items', async function (req, res, next) {
     try 
     {
         let res_login = await baseService.loginSAP(userInfo);
-        let { SessionId } = res_login;
         
+        let { SessionId } = res_login;        
         let cookieString = `B1SESSION=${SessionId}`;
+        
         let res_items = await baseService.getItems(cookieString);
-
         res.json({
             code: true,     
             data: res_items,
             version: info.version,
             time: moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
         });
-
         next();
     } 
     catch(err) {
